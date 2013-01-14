8ee32bfcc3335f7bf0d0bddb9a861de6c9e22c8f
Aaron Rietschlin <nemisis.82@gmail.com>
2013-01-13 18:21:16 -0800

    Fixed some of the styles that are used only API 11+. Also added admob sdk. But am having problems with login credentials.

--- src/com/asa/easysal/ui/PostHCPreferenceActivity.java
+++ src/com/asa/easysal/ui/PostHCPreferenceActivity.java
@@ -1,9 +1,12 @@
 package com.asa.easysal.ui;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.MenuItem;
 
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public class PostHCPreferenceActivity extends FragmentActivity {
 
 	@Override
--- src/com/asa/easysal/ui/PreferencesFragment.java
+++ src/com/asa/easysal/ui/PreferencesFragment.java
@@ -1,11 +1,14 @@
 package com.asa.easysal.ui;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
 import com.asa.easysal.R;
 import com.asa.easysal.SettingsUtil;
 
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class PreferencesFragment extends PreferenceFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
