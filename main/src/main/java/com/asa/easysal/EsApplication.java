c3633ecf17da0ab93c76818096c332f8278aa321
Aaron Rietschlin <aaron.rietschlin@duethealth.com>
2015-07-17 18:44:41 -0400

    Crashlytics

---
+++ main/src/main/java/com/asa/easysal/EsApplication.java
@@ -0,0 +1,14 @@
+package com.asa.easysal;
+
+import android.app.Application;
+
+/**
+ * Created by aaron on 7/17/15.
+ */
+public class EsApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
