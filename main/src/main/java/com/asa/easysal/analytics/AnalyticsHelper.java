b2b8eaa329ffa77b7280a8df82ac45ffc9dce11b
Aaron Rietschlin <aaron.rietschlin@duethealth.com>
2017-08-10 19:58:25 -0400

    WIP: being built with android studio 3.0 and android 26 target API. Need to update Butterknife first.

--- main/src/main/java/com/asa/easysal/analytics/AnalyticsHelper.java
+++ main/src/main/java/com/asa/easysal/analytics/AnalyticsHelper.java
@@ -5,35 +5,16 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.asa.easysal.EsApplication;
-import com.google.android.gms.analytics.HitBuilders;
-import com.google.android.gms.analytics.Tracker;
 
 public class AnalyticsHelper {
 
-    public static Tracker getTracker(Context context) {
-        return ((EsApplication) context.getApplicationContext()).getTracker();
-    }
+    // TODO - This class needs to be re-implemented.
 
     public static void sendScreenView(Context context, @AnalyticsContants.Screens String stringName) {
-        Tracker tracker = getTracker(context);
-        tracker.setScreenName(stringName);
-        tracker.send(new HitBuilders.ScreenViewBuilder().build());
     }
 
     public static void sendEvent(@NonNull Context context, @Nullable String category,
                                  @AnalyticsContants.Events @Nullable String action, @Nullable String label) {
-        Tracker tracker = getTracker(context);
-        HitBuilders.EventBuilder eventBuilder = new HitBuilders.EventBuilder();
-        if (isEmpty(category)) {
-            eventBuilder.setCategory(category);
-        }
-        if (isEmpty(action)) {
-            eventBuilder.setAction(action);
-        }
-        if (isEmpty(label)) {
-            eventBuilder.setLabel(label);
-        }
-        tracker.send(eventBuilder.build());
     }
 
     private static boolean isEmpty(@Nullable String text) {
