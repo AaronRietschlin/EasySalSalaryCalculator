7f65bbb0dc80b04af7f2e8cf699005942db9daad
Aaron Rietschlin <aaron.rietschlin@duethealth.com>
2017-08-10 22:21:03 -0400

    Set up firebase.

--- main/src/main/java/com/asa/easysal/analytics/AnalyticsHelper.java
+++ main/src/main/java/com/asa/easysal/analytics/AnalyticsHelper.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.asa.easysal.EsApplication;
-
 public class AnalyticsHelper {
 
     // TODO - This class needs to be re-implemented.
